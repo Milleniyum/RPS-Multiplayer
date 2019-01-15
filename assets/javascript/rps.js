6b9d645f26e2085f28a1b3cc7cb2f127baf56e18
JDLong <jdlong1980@gmail.com>
2019-01-14 21:16:43 -0500

    updated all files

---
+++ assets/javascript/rps.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCzf2Ve0CYGHjifJOAVcVscfZLqVP34_p4",
+    databaseURL: "https://rpsmultiplayer-b4606.firebaseio.com",
+};
+
+firebase.initializeApp(config);
+
+var db = firebase.database();
